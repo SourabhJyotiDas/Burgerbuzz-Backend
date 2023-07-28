376dc9d7983c1850a70c437a2157cdcb6d2d60d4
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-07-28 21:31:49 +0530

    new

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
-const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+const app = express();
 
 
 dotenv.config({ path: "config/config.env" })
