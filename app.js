2d866dee33adff0f3cc898afda7511a34a5bf54a
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-07-28 21:36:13 +0530

    dasdasd

--- app.js
+++ app.js
@@ -20,16 +20,18 @@ app.use(express.json());
 
 
 
-import path from "path";
-const __dirname = path.resolve();
+// import path from "path";
+// const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "./client/build")))    // deploy only
+// app.use(express.static(path.join(__dirname, "./client/build")))    // deploy only
+
+// app.get('/', async (req, res) => {
+//    res.sendFile(path.join(__dirname, './client/build/index.html'));
+// });
 
 app.get('/', async (req, res) => {
-   res.sendFile(path.join(__dirname, './client/build/index.html'));
+   res.send("<h1>Working Fine</h1>")
 });
-
-
 
 
 
