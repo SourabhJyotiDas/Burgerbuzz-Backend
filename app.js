175d50ff56fbad9a38f6f2b880a7c089869c7a4e
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-11-30 18:55:26 +0530

    Update COrs

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ const app = express();
 
 dotenv.config({ path: "config/config.env" })
 
-app.use(
-   cors({
-      credentials: true,
-      origin: "https://localhost:3000",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-   })
-);
+
 
 app.get('/', async (req, res) => {
    res.send("<h1>Working Fine</h1>")
@@ -23,7 +17,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
-
+app.use(
+   cors({
+      credentials: true,
+      origin: "https://localhost:3000",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+   })
+);
 
 
 
